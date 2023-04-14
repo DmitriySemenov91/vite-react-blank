ff676bfe055177be083db4279b428a1f13d3fd96
dimka <dimka8445484@gmail.com>
2023-04-14 16:00:35 +0200

    initial project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./assets/styles.scss";
+import Typography from "./components/Typography";
+
+const App = () => {
+  return (
+    <>
+      <Typography>Rendered H2!!!</Typography>
+    </>
+  );
+};
+
+export default App;
